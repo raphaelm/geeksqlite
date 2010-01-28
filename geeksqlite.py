793d5e4d34e79f965796333926db276456e28bc5
Raphael Michel <webmaster@raphaelmichel.de>
2010-01-28 17:09:14 +0100

    data refresh; add data; drop tables

--- geeksqlite.py
+++ geeksqlite.py
@@ -36,6 +36,34 @@ def err(text):
 	
 class geeksqliteMain:
 	
+	def gettablename(self, title):
+		dialogTree = gtk.glade.XML(dist.interfacedir+"/tablenamedialog.glade")
+		dlg = dialogTree.get_widget('TableNameDialog')
+		dlg.set_title(title)
+		
+		cb = gtk.combo_box_new_text()
+		cb.set_size_request(240,30)
+		
+		result = self.sql('select name, sql from sqlite_master WHERE type = "table";')
+		for row in self.cursor:
+			n = row[0]
+			#ls.append([n])
+			cb.append_text(n)
+		
+		cb.show()
+		dialogTree.get_widget('TableNameField').put(cb, 5, 25)
+		run = dlg.run()
+		if run == 3:
+			if cb.get_active_text():
+				tablename = cb.get_active_text()
+			else:
+				tablename = None
+			dlg.destroy()
+			return tablename
+		else:
+			dlg.destroy()
+			return None
+		
 	def browse_to(self, table, start=0, limit=50, search=None):
 		self.browse_current_table = table
 		self.browse_current_start = start
@@ -48,13 +76,12 @@ class geeksqliteMain:
 				ef.remove(self.btv)
 				self.btv = False
 			except:
-				print "Error"
+				self.btv = False
 			try:
-				query = "SELECT * FROM "+table;
+				query = "SELECT rowid, * FROM "+table;
 				if search != None:
 					query += " "+search
 				query += " LIMIT "+str(start)+","+str(limit)+";"
-				
 				result = self.sql(query)
 				ls = False
 				C = 0
@@ -79,13 +106,13 @@ class geeksqliteMain:
 						labels[j] = str(k[0])
 						j += 1
 					self.btv = gtk.TreeView(ls)
+					
 					for i in range(0,l):
 						cols[i] = gtk.TreeViewColumn(labels[i])
 						cells[i] = gtk.CellRendererText()
 						cols[i].pack_start(cells[i])
 						cols[i].add_attribute(cells[i], 'text', i)
 						cols[i].set_resizable(True)
-						cols[i].set_reorderable(True)
 						self.btv.append_column(cols[i])
 						self.btv.show()
 					ef.add(self.btv)
@@ -100,7 +127,7 @@ class geeksqliteMain:
 						self.mainTree.get_widget('ScrollLeft').hide()
 					else:
 						self.mainTree.get_widget('ScrollLeft').show()
-					scrolllabel.set_text(str(start)+' to '+str(start+C)+' of '+str(total))
+					scrolllabel.set_text(str(start+1)+' to '+str(start+C)+' of '+str(total))
 			except sqlite3.Error, e:
 				ebuf.set_text('[SQLite Error] '+e.args[0])
 		
@@ -129,8 +156,13 @@ class geeksqliteMain:
 		self.log_add(text)
 		return self.cursor.execute(text)
 		
-	def reloadbrowse(self):
-		pass
+	def reloadbrowse(self, this = None):
+		cb = self.mainTree.get_widget('TableSelect').get_active_text()
+		if cb != None:
+			self.browse_to(table=self.browse_current_table,
+			start=self.browse_current_start,
+			limit=self.browse_current_limit,
+			search=self.browse_current_search)
 		
 	def reloadstructure(self):
 		# Tables
@@ -171,6 +203,17 @@ class geeksqliteMain:
 	def table_create(self, this):
 		pass
 		
+	def table_delete(self, this):
+		tblname = self.gettablename(_('Drop Table'))
+		if tblname:
+			self.sql('DROP TABLE '+tblname)
+			if tblname == self.browse_current_table:
+				self.browse_to('sqlite_master',
+				start=0,
+				limit=0,
+				search=None)
+			self.reloadstructure()
+		
 	def exit(self, this):
 		self.close()
 		gtk.main_quit(self, this)
@@ -200,7 +243,10 @@ class geeksqliteMain:
 	def browse_nextpage(self, this):
 		cb = self.mainTree.get_widget('TableSelect').get_active_text()
 		if cb != None:
-			self.browse_to(table=self.browse_current_table,start=self.browse_current_start+self.browse_current_limit,limit=self.browse_current_limit,search=self.browse_current_search)
+			self.browse_to(table=self.browse_current_table,
+			start=self.browse_current_start+self.browse_current_limit,
+			limit=self.browse_current_limit,
+			search=self.browse_current_search)
 		
 	def browse_previouspage(self, this):
 		cb = self.mainTree.get_widget('TableSelect').get_active_text()
@@ -208,7 +254,10 @@ class geeksqliteMain:
 			start = self.browse_current_start-self.browse_current_limit
 			if start < 0:
 				start = 0
-			self.browse_to(table=self.browse_current_table,start=start,limit=self.browse_current_limit,search=self.browse_current_search)
+			self.browse_to(table=self.browse_current_table,
+			start=start,
+			limit=self.browse_current_limit,
+			search=self.browse_current_search)
 
 	def execute(self, this):
 		if self.fileopened:
@@ -225,7 +274,7 @@ class geeksqliteMain:
 					ef.remove(self.exectv)
 					self.exectv = False
 				except:
-					print "Error"
+					pass
 				try:
 					result = self.sql(inpu)
 					if inpu.lstrip().upper().startswith("SELECT"):
@@ -292,6 +341,24 @@ class geeksqliteMain:
 						f.write('%s\n' % line)
 		else:
 			err('No database loaded')
+			
+	def addemptyrecord(self, this):
+		cb = self.mainTree.get_widget('TableSelect').get_active_text()
+		if cb != None:
+			self.sql('INSERT INTO '+self.browse_current_table+' DEFAULT VALUES')
+			self.reloadbrowse()
+		
+	def deleterecord(self, this):
+		cb = self.mainTree.get_widget('TableSelect').get_active_text()
+		if cb != None:
+			tuple = self.btv.get_selection().get_selected()
+			model = tuple[0]
+			iter = tuple[1]
+			if iter != None:
+				rowid = model.get_value(iter, 0)
+				self.sql('DELETE FROM '+self.browse_current_table+' WHERE rowid = '+rowid)
+			self.reloadbrowse()
+			
 	#### GTK INITIALISATION
 	
 	def grab_widgets(self):
@@ -307,6 +374,7 @@ class geeksqliteMain:
 				"on_MainMenuOpen_activate" : self.open,
 				"on_MainMenuClose_activate" : self.close,
 				"on_MainMenuTableCreate_activate" : self.table_create,
+				"on_MainMenuTableDelete_activate" : self.table_delete,
 				"on_ExecButton_activate" : self.execute,
 				"on_ExecButton_clicked" : self.execute,
 				"on_TableSelect_changed" : self.browse,
@@ -315,7 +383,13 @@ class geeksqliteMain:
 				"on_ScrollRight_activate" : self.browse_nextpage,
 				"on_ScrollRight_clicked" : self.browse_nextpage,
 				"on_ScrollLeft_activate" : self.browse_previouspage,
-				"on_ScrollLeft_clicked" : self.browse_previouspage
+				"on_ScrollLeft_clicked" : self.browse_previouspage,
+				"on_ReloadButton_activate" : self.reloadbrowse,
+				"on_ReloadButton_clicked" : self.reloadbrowse,
+				"on_AddButton_activate" : self.addemptyrecord,
+				"on_AddButton_clicked" : self.addemptyrecord,
+				"on_DeleteButton_activate" : self.deleterecord,
+				"on_DeleteButton_clicked" : self.deleterecord,
 			  }
 		self.mainTree.signal_autoconnect(dic)
 	
@@ -357,6 +431,7 @@ class geeksqliteMain:
 		self.connect_events()	
 		self.design_treeviews()
 		self.fileopened = False
+		self.browse_current_table = None
 		
 		if len(sys.argv) == 2:
 			if sys.argv[1] in ('-h', '--help'):
