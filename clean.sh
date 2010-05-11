061bb25a448a760b087a97b34d3d404c2ddb34e0
Raphael Michel <webmaster@raphaelmichel.de>
2010-05-11 18:40:56 +0200

    Manpage improved

--- clean.sh
+++ clean.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 rm -f language/*/LC_MESSAGES/geeksqlite.mo
+gunzip geeksqlite.1.gz
--- make.sh
+++ make.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 
-# Detects and parses the architecture
-ARCH=$(uname -m)
-case "$ARCH" in
- "i686") ARCH="i386";;
- "i586") ARCH="i386";;
- "i486") ARCH="i386";;
-esac
-
-echo "Target architecture: $ARCH"
-OS="linux"
-echo "Target operating system: $OS"
-
 # BUILD
+
 # localisation
 msgfmt language/de/LC_MESSAGES/geeksqlite.po -o language/de/LC_MESSAGES/geeksqlite.mo
 msgfmt language/en/LC_MESSAGES/geeksqlite.po -o language/en/LC_MESSAGES/geeksqlite.mo
 msgfmt language/eo/LC_MESSAGES/geeksqlite.po -o language/eo/LC_MESSAGES/geeksqlite.mo
+
+# manpage
+gzip geeksqlite.1
